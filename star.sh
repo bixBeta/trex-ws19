a4709b13ed711dc12cee1da0f99200913640e3a4
faraz <33409348+bixBeta@users.noreply.github.com>
2019-11-18 18:31:05 -0500

    Update star.sh

--- star.sh
+++ star.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
 
+for i in *_trimmed.fastq.gz
+
+	    do
+      
+	        STAR \
+	        --runThreadN 6 \
+	        --genomeDir /workdir/genome/GRCh38.star \
+	        --readFilesIn $i \
+	        --readFilesCommand gunzip -c \
+	        --outSAMstrandField intronMotif \
+	        --outFilterIntronMotifs RemoveNoncanonical \
+	        --outSAMtype BAM SortedByCoordinate \
+	        --outFileNamePrefix $(basename $i trimmed.fastq.gz) \
+	        --quantMode GeneCounts
+
+	    done
+
